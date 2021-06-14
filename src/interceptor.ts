ee5ae597f32a4ef64148f507b9db5041be374e4b
二刺螈 <i@songe.li>
2021-06-14 15:15:30 +0800

    更换路由拒绝的提示

--- src/interceptor.ts
+++ src/interceptor.ts
@@ -2,7 +2,7 @@
  * @Date: 2021-03-18 15:48:07
  * @LastEditors: lisonge
  * @Author: lisonge
- * @LastEditTime: 2021-03-19 18:12:11
+ * @LastEditTime: 2021-06-14 15:10:06
  */
 
 import {
@@ -16,27 +16,33 @@ import { URL } from 'url';
 import { BaseError } from './core/error';
 import { Response } from 'node-fetch';
 
-export const corsAfterFunc: AfterForwardFunc = (latestResp) => {
-  const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers': '*',
-  } as { [key: string]: string };
-  for (const key in headers) {
-    latestResp.headers.set(key, headers[key]);
-  }
-  return Next.from(latestResp);
-};
-export const routeFilterFunc: BeforeForwardFunc = (latestResp, _) => {
+/**
+ * 控制路由访问
+ * @param latestResp
+ * @returns
+ */
+export const routeFilterFunc: BeforeForwardFunc = (latestResp) => {
   const u = new URL(latestResp.url);
   if (
     config.allow_route_list.length > 0 &&
     !config.allow_route_list.some((v) => RegExp(v).test(u.pathname))
   ) {
-    throw BaseError.from(403, 'block invalid path');
+    return End.from(
+      new Response(
+        JSON.stringify({ message: 'block invalid path' }, undefined, 2),
+        {
+          status: 403,
+        }
+      )
+    );
   }
 };
 
+/**
+ * 控制预检请求
+ * @param latestReq
+ * @returns
+ */
 export const corsBeforFunc: BeforeForwardFunc = (latestReq) => {
   if (latestReq.method === 'OPTIONS') {
     return End.from(
@@ -49,4 +55,21 @@ export const corsBeforFunc: BeforeForwardFunc = (latestReq) => {
       })
     );
   }
+};
+
+/**
+ * 跨域控制
+ * @param latestResp
+ * @returns
+ */
+export const corsAfterFunc: AfterForwardFunc = (latestResp) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Headers': '*',
+  } as { [key: string]: string };
+  for (const key in headers) {
+    latestResp.headers.set(key, headers[key]);
+  }
+  return Next.from(latestResp);
 };
