e781e5662e6149d7bc2eaf8e29e01bff75923f11
二刺螈 <i@songe.li>
2021-06-14 15:13:58 +0800

    类型声明: 更改 interface 为 type

--- src/@types/aliyun.ts
+++ src/@types/aliyun.ts
@@ -2,65 +2,68 @@
  * @Date: 2021-02-21 19:52:00
  * @LastEditors: lisonge
  * @Author: lisonge
- * @LastEditTime: 2021-03-18 12:49:27
+ * @LastEditTime: 2021-06-14 14:51:46
  */
 
 import stream from 'stream';
 import internal from 'stream';
 
 /**
- * @see <https://help.aliyun.com/document_detail/156876.html>
+ * @see <https://help.aliyun.com/document_detail/156876.htm#section-mcp-kix-v2j>
  */
-export interface AliyunContext {
+export type AliyunContext = {
   requestId: string;
   credentials: Credentials;
   function: Function;
   service: Service;
   region: string;
   accountId: string;
-}
-interface Credentials {
+};
+type Credentials = {
   accessKeyId: string;
   accessKeySecret: string;
   securityToken: string;
-}
-interface Service {
+};
+type Service = {
   name: string;
   logProject: string;
   logStore: string;
   qualifier: string;
   versionId: string;
-}
+};
 
 /**
- * @see <https://help.aliyun.com/document_detail/74757.html>
+ * @see <https://help.aliyun.com/document_detail/74757.html#section-960-nx8-b4i>
  */
-export interface AliyunRequest extends internal.Readable {
+export type AliyunRequest = internal.Readable & {
   headers: { [key: string]: string };
   path: string;
   queries: { [key: string]: string };
   url: string;
   clientIP: string;
   method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'HEAD' | 'PATCH';
-}
-
-export interface AliyunResponse {
+};
+/**
+ * @see <https://help.aliyun.com/document_detail/74757.html#section-t19-6rk-hs5>
+ */
+export type AliyunResponse = {
   setStatusCode: (statusCode: number) => void;
   setHeader: (headerKey: string, headerValue: string) => void;
   deleteHeader: (headerKey: string) => void;
   send: (body: string | stream.Readable | Buffer) => void;
-}
+};
 
-interface HandledInvocationError extends Error {
+type HandledInvocationError = Error & {
   // i do not know what its [internal realization] is
-}
+};
 
 /**
  * @see <https://help.aliyun.com/document_detail/156876.html>
  */
-export interface AliyunCallback {
+export type AliyunCallback = {
   (error: null, data: Buffer): Buffer;
   (error: null, data: object): string;
   (error: null, data: Exclude<any, Buffer | object>): string;
   (error: NonNullable<any>, data: any): HandledInvocationError;
-}
+};
+
